6b67067e702e7d896a61f5dc8b6117cb9c4a59b8
Mohit5039 <mohitshekhawat2323@gmail.com>
2025-10-07 18:35:32 +0530

    name change

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from llm import ask_career_bot
 
 # ----------------- Config --------------------
 st.set_page_config(
-    page_title="Glassbroken: AI Career Buddy",
+    page_title="Mārga: AI-Powered Career Guide",
     layout="centered"
 )
 
